ff7e2343eaec3c7d950d648dbec8f59fa3fb057e
Dhruv <dhruvvv099@gmail.com>
2025-07-22 22:18:25 +0530

    header profile

--- src/app/(root)/_components/HeaderProfileBtn.tsx
+++ src/app/(root)/_components/HeaderProfileBtn.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+import { SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
 
 function HeaderProfileBtn() {
   return (
-    <div>HeaderProfileBtn</div>
-  )
-}
+    <>
+      <UserButton>
+        <UserButton.MenuItems>
+          <UserButton.Link
+            label="Profile"
+            labelIcon={<User className="size-4" />}
+            href="/profile"
+          />
+        </UserButton.MenuItems>
+      </UserButton>
 
-export default HeaderProfileBtn
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </>
+  );
+}
+export default HeaderProfileBtn;
