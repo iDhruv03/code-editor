8221716b9acaacb5b3eaa67b10a94f278e26a292
Dhruv <dhruvvv099@gmail.com>
2025-07-22 22:03:58 +0530

    logo

--- src/app/(root)/_components/Header.tsx
+++ src/app/(root)/_components/Header.tsx
@@ -1,6 +1,8 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { ConvexHttpClient } from "convex/browser";
 import { api } from "../../../../convex/_generated/api";
+import Link from "next/link";
+import { Blocks } from "lucide-react";
 
 async function Header(){
   const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
@@ -10,8 +12,43 @@ async function Header(){
     userId: user?.id || ""
   })
 
-  console.log({convexUser});
+  
 
-  return <div>Header</div>
+  return (
+    <div className="relative z-10">
+      <div
+        className="flex items-center lg:justify-between justify-center 
+        bg-[#0a0a0f]/80 backdrop-blur-xl p-6 mb-4 rounded-lg">
+          <div className="hidden lg:flex items-center gap-8">
+            <Link href="/" className="flex items-center gap-3 group relative">
+            {/* Logo hover effect */}
+
+            <div
+              className="absolute -inset-2 bg-gradient-to-r from-blue-500/20 to-purple-500/20 rounded-lg opacity-0 
+                group-hover:opacity-100 transition-all duration-500 blur-xl"
+            />
+
+            {/* Logo */}
+            <div
+              className="relative bg-gradient-to-br from-[#1a1a2e] to-[#0a0a0f] p-2 rounded-xl ring-1
+              ring-white/10 group-hover:ring-white/20 transition-all"
+            >
+              <Blocks className="size-6 text-blue-400 transform -rotate-6 group-hover:rotate-0 transition-transform duration-500" />
+            </div>
+
+            <div className="flex flex-col">
+              <span className="block text-lg font-semibold bg-gradient-to-r from-blue-400 via-blue-300 to-purple-400 text-transparent bg-clip-text">
+                CodeCraft
+              </span>
+              <span className="block text-xs text-blue-400/60 font-medium">
+                Interactive Code Editor
+              </span>
+            </div>
+          </Link>
+          </div>
+        </div>
+
+    </div>
+    )
 }
 export default Header;
