01da96cc660003996013781f57134eb6ce2363f3
Dhruv <dhruvvv099@gmail.com>
2025-07-24 18:30:31 +0530

    event listeners for theme selection

--- src/app/(root)/_components/ThemeSelector.tsx
+++ src/app/(root)/_components/ThemeSelector.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import { useCodeEditorStore } from "@/store/useCodeEditorStore";
-import { useRef, useState } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import { THEMES } from "../_constants";
 
 function ThemeSelector () {
   const[isOpen, setIsOpen] = useState(false);
   const{theme,setTheme }= useCodeEditorStore();
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const currentTheme = THEMES.find((t) => t.id === theme);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+
   return <div>ThemeSelector</div>
 }
 export default ThemeSelector;
